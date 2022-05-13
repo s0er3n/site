8b33e77ab2de81f3cd8ee9ad6a8adb16bed82e85
Sören Michaels <soeren.michaels@gmail.com>
2022-05-14 01:29:35 +0200

    init

---
+++ src/GeoBingoLogo.svelte
@@ -0,0 +1,99 @@
+<svg viewBox="0 0 548 548" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g filter="url(#filter0_d_4_43)">
+		<ellipse
+			cx="273.699"
+			cy="273.532"
+			rx="240.2"
+			ry="240"
+			transform="rotate(-8.6887 273.699 273.532)"
+			fill="#3F48CC"
+		/>
+	</g>
+	<g filter="url(#filter1_d_4_43)">
+		<path
+			d="M101.182 234.869C104.349 241.86 118.435 265.335 121.672 265.909C123.999 266.321 145.5 269.034 159.154 270.739C163.689 272.934 179.57 272.437 206.819 252.897C215.095 245.225 225.673 245.744 281.409 253.244C337.146 260.744 286.539 252.46 341.165 204.602C352.497 182.582 341.234 198.185 334.494 126.602C330.943 75.8891 343.475 112.63 341.501 99.7103C339.97 89.6931 338.868 79.6612 338.284 75.8352C321.84 75.8565 283.048 75.7337 259.43 75.0715C229.909 74.2439 130.367 86.9152 122.639 91.2997C116.456 94.8074 95.7896 164.456 59.615 200.239C63.7858 209.212 98.6472 229.277 101.182 234.869Z"
+			fill="#22B14C"
+		/>
+		<path
+			d="M283.031 435.595C279.634 433.978 270.949 432.102 249.528 360.628C250.896 360.418 252.141 355.743 246.179 338.715C238.727 317.429 244.585 328.28 245.977 309.914C247.37 291.548 249.99 301.826 264.172 284.709C278.353 267.593 274.728 271.35 295.474 262.841C316.22 254.331 301.47 260.856 320.095 259.078C338.72 257.3 325.543 260.381 381.418 255.046C437.293 249.71 393.888 254.208 417.387 291.194C440.886 328.181 406.852 318.432 401.425 331.008C395.998 343.583 395.769 348.957 391.368 361.376C386.967 373.795 388.312 375.725 379.031 397.432C369.751 419.139 363.186 410.532 361.613 413.975C360.04 417.419 371.824 432.704 399.793 450.854C422.169 465.374 411.561 472.904 403.461 474.854L378.84 478.616C361.105 477.055 317.113 466.265 283.031 435.595Z"
+			fill="#22B14C"
+		/>
+	</g>
+	<g filter="url(#filter2_d_4_43)">
+		<path
+			d="M350.873 83.855L367.313 52.4968C385.604 64.1246 416.678 82.2122 448.828 107.347C480.979 132.481 457.567 133.655 471.53 155.56C485.494 177.464 488.082 186.684 499.485 214.989C510.888 243.294 508.212 242.982 507.023 264.317C506.234 278.488 508.682 281.55 509.372 281.263C509.426 281.094 509.481 280.927 509.536 280.76C509.576 281.019 509.508 281.206 509.372 281.263C498.218 315.635 495.385 386.633 488.598 375.305C480.681 362.092 486.595 346.765 483.197 231.901L481.94 223.68L451.074 222.387C446.404 222.7 430.669 218.534 405.092 199.366L401.502 175.877C399.708 164.132 388.795 162.194 382.314 158.376C375.833 154.559 368.53 137.647 364.969 129.778C362.119 123.482 356.897 112.982 354.643 108.519L350.873 83.855Z"
+			fill="#22B14C"
+		/>
+	</g>
+	<defs>
+		<filter
+			id="filter0_d_4_43"
+			x="29.4703"
+			y="33.4937"
+			width="488.458"
+			height="488.076"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset dy="4" />
+			<feGaussianBlur stdDeviation="2" />
+			<feComposite in2="hardAlpha" operator="out" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4_43" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4_43" result="shape" />
+		</filter>
+		<filter
+			id="filter1_d_4_43"
+			x="55.615"
+			y="75.0328"
+			width="373.955"
+			height="411.583"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset dy="4" />
+			<feGaussianBlur stdDeviation="2" />
+			<feComposite in2="hardAlpha" operator="out" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4_43" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4_43" result="shape" />
+		</filter>
+		<filter
+			id="filter2_d_4_43"
+			x="346.873"
+			y="52.4968"
+			width="166.674"
+			height="332.02"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset dy="4" />
+			<feGaussianBlur stdDeviation="2" />
+			<feComposite in2="hardAlpha" operator="out" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4_43" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4_43" result="shape" />
+		</filter>
+	</defs>
+</svg>
---
+++ src/routes/index.svelte
@@ -0,0 +1,95 @@
+<script>
+	import Discord from '../Discord.svelte';
+
+	import { CountUp } from 'countup.js';
+
+	function initCount(node, count) {
+		const countUp = new CountUp(node, count, { enableScrollSpy: true });
+		if (!countUp.error) {
+			// countUp.start();
+		} else {
+			console.error(countUp.error);
+		}
+	}
+	///@ts-ignore
+	import Typewriter from 'typewriter-effect/dist/core.js';
+	import Github from '../Github.svelte';
+	import Chevron from '../Chevron.svelte';
+	import GeoBingoLogo from '../GeoBingoLogo.svelte';
+
+	function init(node) {
+		new Typewriter(node, {
+			strings: ['Sören Michaels', 'Fullstack Developer'],
+			loop: true,
+			autoStart: true
+		});
+	}
+</script>
+
+<div class="flex flex-col h-screen">
+	<div class="flex justify-around items-center h-full">
+		<div class=" text-center text-6xl text-white w-full">
+			<div class="" use:init />
+		</div>
+		<div class="bg-white pl-[1px] h-96" />
+		<div class="flex justify-center  text-white w-full grid space-y-16">
+			<div class="flex items-center justify-center space-x-4">
+				<div class="w-12 h-12">
+					<Discord />
+				</div>
+				<span class="text-4xl">
+					<span class="font-bold">Sören</span><span class="font-thin">#1141</span>
+				</span>
+			</div>
+			<a href="https://github.com/s0er3n" target="_blank">
+				<div class="flex items-center justify-center space-x-4">
+					<div class="w-12 h-12">
+						<Github />
+					</div>
+					<span class="text-4xl">
+						<span class="font-thin">github.com/</span><span class="font-bold">s0er3n</span>
+					</span>
+				</div>
+			</a>
+			<a href="https://ko-fi.com/soerenmichaels" target="_blank">
+				<div class="flex items-center justify-center space-x-4">
+					<!-- <div class="w-48 h-12"> -->
+					<!-- 	<!-- <img src="kofi.png" /> -->
+					<!-- </div> -->
+					<span class="text-4xl">
+						<span class="font-thin">ko-fi.com/</span><span class="font-bold">soerenmichaels</span>
+					</span>
+				</div>
+			</a>
+		</div>
+	</div>
+	<a href="/#geobingo">
+		<div class="flex h-8 text-white w-full justify-center">
+			<Chevron />
+		</div>
+	</a>
+</div>
+<!-- second part  -->
+<div id="geobingo" class=" h-screen flex font-mono flex-col text-center text-white">
+	<div class="p-5 text-6xl font-bold  w-full text-center ">GeoBingo.io</div>
+	<div class="flex h-full justify-around items-center">
+		<div class="flex flex-col justify-center w-full h-full">
+			<GeoBingoLogo />
+		</div>
+		<div class="grid space-x-4 w-full">
+			<p class="text-6xl p-4 font-thin">
+				<span class=" font-bold">~<span use:initCount={30} />k</span> monthly players
+			</p>
+			<p class="text-3xl p-4 font-thin">
+				over <span class=" font-bold" use:initCount={400} />K views on YouTube
+			</p>
+			<p class="text-xl p-2 font-thin">without advertising in under half a year</p>
+			<p class="text-2xl p-4 pt-24 font-thin">
+				build with <span class="font-bold"> Typescript</span> and
+			</p>
+			<p class="text-2xl p-4 font-bold">
+				SvelteKit NodeJS Tailwindcss Socket.io Docker Google Maps API
+			</p>
+		</div>
+	</div>
+</div>
