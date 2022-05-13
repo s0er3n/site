4c5f7e765255525d11451b15f481bfa91b9c182d
Sören Michaels <soeren.michaels@gmail.com>
2022-05-14 01:36:38 +0200

    reverted config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,10 +8,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    paths: {
-      // change below to your repo name
-      base: "/site",
-    }, adapter: adapter()
+    adapter: adapter()
   }
 };
 
